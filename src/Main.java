641e9742daf8c3176f5b54f5ee5a7bddc0150465
LessiaSa <0162221@mail.ru>
2024-05-24 10:41:16 +0700

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет, Нетология!");
+        System.out.println("2+3");
+        System.out.println(2+3);
+
+        int speed=17;
+        int x=speed+3;
+
+        double d=6.5;
+
+        String name="Petya";
+        System.out.println(name +" ехал со скоростью " + speed);
+    }
+}
